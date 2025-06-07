c6e722ae402515f691a1d9731d8fb86e1f6580f3
ADITYA SINGH <119720155+EchoSingh@users.noreply.github.com>
2025-06-07 21:19:35 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,11 +9,10 @@ const {
 } = process.env;
 
 const wakatime = new WakaTimeClient(wakatimeApiKey);
-
 const octokit = new Octokit({ auth: `token ${githubToken}` });
 
 async function main() {
-  const stats = await wakatime.getMyStats({ range: RANGE.ALL_TIME });
+  const stats = await wakatime.getMyStats({ range: RANGE.LAST_7_DAYS });
   await updateGist(stats);
 }
 
@@ -28,12 +27,12 @@ async function updateGist(stats) {
     gist = await octokit.gists.get({ gist_id: gistId });
   } catch (error) {
     console.error(`Unable to get gist\n${error}`);
+    return;
   }
 
   const lines = [];
   for (let i = 0; i < Math.min(stats.data.languages.length, 5); i++) {
-    const data = stats.data.languages[i];
-    const { name, percent, text: time } = data;
+    const { name, percent, text: time } = stats.data.languages[i];
 
     const line = [
       trimRightStr(name, 10).padEnd(10),
@@ -45,16 +44,15 @@ async function updateGist(stats) {
     lines.push(line.join(" "));
   }
 
-  if (lines.length == 0) return;
+  if (lines.length === 0) return;
 
   try {
-    // Get original filename to update that same file
     const filename = Object.keys(gist.data.files)[0];
     await octokit.gists.update({
       gist_id: gistId,
       files: {
         [filename]: {
-          filename: `📊 Waka Time Stats `,
+          filename: `📈 Weekly Development Progress Bar`,
           content: lines.join("\n")
         }
       }
@@ -66,17 +64,17 @@ async function updateGist(stats) {
 
 function generateBarChart(percent, size) {
   const syms = "░▏▎▍▌▋▊▉█";
-
   const frac = Math.floor((size * 8 * percent) / 100);
   const barsFull = Math.floor(frac / 8);
-  if (barsFull >= size) {
-    return syms.substring(8, 9).repeat(size);
-  }
-  const semi = frac % 8;
 
-  return [syms.substring(8, 9).repeat(barsFull), syms.substring(semi, semi + 1)]
+  if (barsFull >= size) {
+    return syms[8].repeat(size);
+  }
+
+  const semi = frac % 8;
+  return [syms[8].repeat(barsFull), syms[semi]]
     .join("")
-    .padEnd(size, syms.substring(0, 1));
+    .padEnd(size, syms[0]);
 }
 
 (async () => {
